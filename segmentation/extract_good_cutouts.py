f4c22829fd194a6cfe380b1c8ac832ce054c79e7
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-12-19 22:15:24 +0100

    Allow GCP upload

--- segmentation/extract_good_cutouts.py
+++ segmentation/extract_good_cutouts.py
@@ -2,18 +2,27 @@ import argparse
 import functools
 from pathlib import Path
 import multiprocessing
+import sys
 
 import tqdm.auto
 
+from segmentation import gcp
 from segmentation.cutting import extract_cutouts
 
 
 def save_cutouts_for_index(
-    image_index: int, output_dir: Path, error_on_missing_file: bool = True
+    image_index: int,
+    output_dir: Path,
+    error_on_missing_file: bool = True,
+    gcp_prefix: str | None = None,
 ):
     try:
         for cutout_index, cutout in extract_cutouts(image_index):
-            cutout.save(output_dir / f"{image_index:08d}_{cutout_index:05d}.webp")
+            filename = f"{image_index:08d}_{cutout_index:05d}.webp"
+            cutout.save(output_dir / filename)
+
+            if gcp_prefix:
+                gcp.upload_blob(output_dir / filename, gcp_prefix + filename)
     except FileNotFoundError:
         if error_on_missing_file:
             raise
@@ -23,10 +32,12 @@ def save_cutouts_for_index(
             return
 
 
-def main(output_dir: Path, max_index: int):
+def main(output_dir: Path, max_index: int, gcp_prefix: str | None = None):
     output_dir.mkdir(exist_ok=True)
 
-    f = functools.partial(save_cutouts_for_index, output_dir=args.output_dir)
+    f = functools.partial(
+        save_cutouts_for_index, output_dir=output_dir, gcp_prefix=gcp_prefix
+    )
 
     with multiprocessing.Pool(8) as pool:
         pool.map(f, tqdm.auto.trange(1, max_index + 1), chunksize=1)
@@ -36,6 +47,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output-dir", "-o", type=Path, required=True)
     parser.add_argument("--max-index", type=int, default=100)
+    parser.add_argument("--gcp-prefix", type=str, default=None)
     args = parser.parse_args()
 
-    main(args.output_dir, args.max_index)
+    gcp_prefix = args.gcp_prefix
+    if gcp_prefix is not None:
+        if not gcp_prefix.startswith("cutouts/v"):
+            print("gcp-prefix should start with 'cutouts/v1' or a different version")
+            sys.exit(1)
+
+        if not gcp_prefix.endswith("/"):
+            print("gcp-prefix should end with a slash, adding automatically")
+            gcp_prefix += "/"
+
+    main(output_dir=args.output_dir, max_index=args.max_index, gcp_prefix=gcp_prefix)
---
+++ segmentation/gcp.py
@@ -0,0 +1,97 @@
+"""
+When interacting with Google Cloud Client libraries, the library can auto-detect the
+credentials to use.
+
+1. Before running this sample, set up ADC as described in
+   https://cloud.google.com/docs/authentication/external/set-up-adc
+2. Make sure that the user account or service account that you are using
+   has the required permissions. For this sample, you must have "storage.buckets.list".
+"""
+from pathlib import Path
+from google.cloud import storage
+from google.cloud.storage import Client, transfer_manager
+
+GCP_PROJECT_ID = "vv-segmentation"
+BUCKET_ID = "vv-segmentation"
+
+
+def authenticate_implicit_with_adc():
+    # This snippet demonstrates how to list buckets.
+    # *NOTE*: Replace the client created below with the client required for your application.
+    # Note that the credentials are not specified when constructing the client.
+    # Hence, the client library will look for credentials using ADC.
+    storage_client = storage.Client(project=GCP_PROJECT_ID)
+    buckets = storage_client.list_buckets()
+    print("Buckets:")
+    for bucket in buckets:
+        print(bucket.name)
+    print("Listed all storage buckets.")
+
+
+def upload_many_blobs_with_transfer_manager(
+    bucket_name, filenames, source_directory="", workers=8
+):
+    """Upload every file in a list to a bucket, concurrently in a process pool.
+
+    Each blob name is derived from the filename, not including the
+    `source_directory` parameter. For complete control of the blob name for each
+    file (and other aspects of individual blob metadata), use
+    transfer_manager.upload_many() instead.
+    """
+
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # A list (or other iterable) of filenames to upload.
+    # filenames = ["file_1.txt", "file_2.txt"]
+
+    # The directory on your computer that is the root of all of the files in the
+    # list of filenames. This string is prepended (with os.path.join()) to each
+    # filename to get the full path to the file. Relative paths and absolute
+    # paths are both accepted. This string is not included in the name of the
+    # uploaded blob; it is only used to find the source files. An empty string
+    # means "the current working directory". Note that this parameter allows
+    # directory traversal (e.g. "/", "../") and is not intended for unsanitized
+    # end user input.
+    # source_directory=""
+
+    # The maximum number of processes to use for the operation. The performance
+    # impact of this value depends on the use case, but smaller files usually
+    # benefit from a higher number of processes. Each additional process occupies
+    # some CPU and memory resources until finished. Threads can be used instead
+    # of processes by passing `worker_type=transfer_manager.THREAD`.
+    # workers=8
+
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    results = transfer_manager.upload_many_from_filenames(
+        bucket, filenames, source_directory=source_directory, max_workers=workers
+    )
+
+    for name, result in zip(filenames, results):
+        # The results list is either `None` or an exception for each filename in
+        # the input list, in order.
+
+        if isinstance(result, Exception):
+            print("Failed to upload {} due to exception: {}".format(name, result))
+        else:
+            print("Uploaded {} to {}.".format(name, bucket.name))
+
+
+def upload_blob(source_file_name: str | Path, destination_blob_name: str):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_ID)
+
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(str(source_file_name))
+
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
